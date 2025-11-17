f890d277e72b96d93542a4746e6d10b08112c081
Pavel Strakhov <ri@idzaaus.org>
2025-11-17 00:37:06 +0000

    add android ci

--- android/README.md
+++ android/README.md
@@ -1,8 +1,1 @@
-Add Rust targets:
-```
-rustup target add \
-    aarch64-linux-android \
-    armv7-linux-androideabi \
-    i686-linux-android \
-    x86_64-linux-android
-```
+# Rammingen for Android
--- android/native/build.sh
+++ android/native/build.sh
@@ -12,15 +12,19 @@ fi
 export AR="$ANDROID_TOOLCHAINS/llvm-ar"
 
 export CC="$ANDROID_TOOLCHAINS/aarch64-linux-android30-clang"
+export CARGO_TARGET_AARCH64_LINUX_ANDROID_LINKER="$CC"
 cargo build --target aarch64-linux-android --release
 
 export CC="$ANDROID_TOOLCHAINS/armv7a-linux-androideabi30-clang"
+export CARGO_TARGET_ARMV7_LINUX_ANDROIDEABI_LINKER="$CC"
 cargo build --target armv7-linux-androideabi --release
 
 export CC="$ANDROID_TOOLCHAINS/i686-linux-android30-clang"
+export CARGO_TARGET_I686_LINUX_ANDROID_LINKER="$CC"
 cargo build --target i686-linux-android --release
 
 export CC="$ANDROID_TOOLCHAINS/x86_64-linux-android30-clang"
+export CARGO_TARGET_X86_64_LINUX_ANDROID_LINKER="$CC"
 cargo build --target x86_64-linux-android --release
 
 unset CC
@@ -41,7 +45,3 @@ cp ../../target/i686-linux-android/release/librammingen_android.so \
     "$LIB_PATH/x86/"
 cp ../../target/x86_64-linux-android/release/librammingen_android.so \
     "$LIB_PATH/x86_64/"
-
-#cd ..
-#rm -r app/build
-#gradle build
